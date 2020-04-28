e9b39ce4ec602690c3963da4cf60aa47d265db86
Volodymyr <volodymyr.miroshnychenko@chromeriver.com>
2020-04-28 11:25:07 +0300

    connect mariaDB to project

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'car_rental.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'rental_db',
+            'USER': 'rental_db_admin',
+            'PASSWORD': 'rentadmin',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
 }
 
 
