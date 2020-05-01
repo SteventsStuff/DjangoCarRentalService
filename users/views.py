9e109f25e0cac7569c214bfa736d7f3253e842a0
Volodymyr <volodymyr.miroshnychenko@chromeriver.com>
2020-05-01 17:06:38 +0300

    create view for registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import UserRegistrationForm
+
+
+def register(request):
+    """
+    Note about messages:
+        - message.debug
+        - message.info
+        - message.success
+        - message.warning
+        - message.error
+    """
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account crated for {username}!')
+            return redirect('rental-home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
