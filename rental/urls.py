fa83694797cf4cc1cbf38f7be607423de7ddbd27
Volodymyr <volodymyr.miroshnychenko@chromeriver.com>
2020-05-11 18:45:58 +0300

    add new urls

--- rental/urls.py
+++ rental/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
 from . import views
-
+from .views import (
+    CarRentalListView, CarDetailView, CarCreateView, CarUpdateView, CarDeleteView, CarMarkListView
+)
 
 urlpatterns = [
-    path('', views.index, name='rental-home'),
+    path('', views.home, name='rental-home'),
+    path('catalog/', CarRentalListView.as_view(), name='rental-catalog'),
+    path('car/mark/<str:mark_title>', CarMarkListView.as_view(), name='car-mark'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
+    path('car/<int:pk>/update', CarUpdateView.as_view(), name='car-update'),
+    path('car/<int:pk>/delete', CarDeleteView.as_view(), name='car-delete'),
+    path('car/new/', CarCreateView.as_view(), name='car-create'),
     path('about/', views.about, name='rental-about'),
 ]
