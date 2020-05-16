49108ecb9d4fbf97dd1dcaec7ee4a0cc2143ad5b
Volodymyr <volodymyr.miroshnychenko@chromeriver.com>
2020-05-16 16:15:24 +0300

    create Driver views

--- rental/views.py
+++ rental/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from .models import Car, CarMark
+from .models import Car, CarMark, Driver
 
 
 class CarRentalListView(ListView):
@@ -76,12 +76,61 @@ class CarUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     #     pass
 
 
+class DriversListView(ListView):
+    model = Driver
+    template_name = 'rental/drivers/drivers-catalog.html'
+    context_object_name = 'drivers'
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = 'rental/drivers/drivers-detail.html'
+    context_object_name = 'driver'
+
+
+class DriverCreateView(LoginRequiredMixin, CreateView):
+    model = Driver
+    fields = ['first_name', 'second_name', 'work_experience', 'price']
+    template_name = 'rental/cars/edit/car_form.html'
+
+    # def form_valid(self, form):
+    #     # form.instance.
+    #     # return super().form_valid(form)
+    #     pass
+
+
+class DriverUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Driver
+    fields = ['first_name', 'second_name', 'work_experience', 'price']
+    template_name = 'rental/drivers/edit/driver_form.html'
+
+    def test_func(self):
+        return True
+        # car = self.get_object()
+        # if self.request.user == car.author:
+        #     return True
+        # return False
+
+    # def form_valid(self, form):
+    #     # form.instance.
+    #     # return super().form_valid(form)
+    #     pass
+
+
+class DriverDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Driver
+    context_object_name = 'driver'
+    success_url = '/'
+    template_name = 'rental/drivers/edit/driver_confirm_delete.html'
+
+    def test_func(self):
+        return True
+
+
 def home(request):
     return render(request, 'rental/home.html')
 
 
 def about(request):
-    context = {
-        'title': 'kek'
-    }
-    return render(request, 'rental/about.html', context)
+    return render(request, 'rental/about.html')
