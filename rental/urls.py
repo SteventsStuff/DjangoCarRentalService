e1500404f003a5c40df7db447664387ddb407f12
Volodymyr <volodymyr.miroshnychenko@chromeriver.com>
2020-05-16 17:07:27 +0300

    add new urls for creating new marks, conditions and classes

--- rental/urls.py
+++ rental/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from .views import (
     CarRentalListView, CarDetailView, CarCreateView, CarUpdateView, CarDeleteView, CarMarkListView,
+    CarMarkCreateView, CarConditionCreateView, CarClassCreateView,
     DriversListView, DriverDetailView, DriverCreateView, DriverUpdateView, DriverDeleteView
 )
 
@@ -12,11 +13,18 @@ urlpatterns = [
     path('about/', views.about, name='rental-about'),
     # cars
     path('car-catalog/', CarRentalListView.as_view(), name='car-catalog'),
-    path('car/mark/<str:mark_title>', CarMarkListView.as_view(), name='car-mark'),
     path('car/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
     path('car/<int:pk>/update', CarUpdateView.as_view(), name='car-update'),
     path('car/<int:pk>/delete', CarDeleteView.as_view(), name='car-delete'),
     path('car/new/', CarCreateView.as_view(), name='car-create'),
+    # edit car marks
+    path('car-mark/new/', CarMarkCreateView.as_view(), name='car-mark-create'),
+    # edit car condition
+    path('car-condition/new/', CarConditionCreateView.as_view(), name='car-condition-create'),
+    # edit car class
+    path('car-class/new/', CarClassCreateView.as_view(), name='car-class-create'),
+    # cars: filters
+    path('car/mark/<str:mark_title>', CarMarkListView.as_view(), name='car-mark'),
     # drivers
     path('driver-catalog/', DriversListView.as_view(), name='driver-catalog'),
     path('driver/<int:pk>/', DriverDetailView.as_view(), name='driver-detail'),
@@ -24,4 +32,5 @@ urlpatterns = [
     path('driver/<int:pk>/delete/', DriverDeleteView.as_view(), name='driver-delete'),
     path('driver/<int:pk>/update/', DriverUpdateView.as_view(), name='driver-update'),
 ]
+
 
